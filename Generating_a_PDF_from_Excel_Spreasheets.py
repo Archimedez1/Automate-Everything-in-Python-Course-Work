02bee6cb5d633951e0353a95957c14c5dcfc9357
Archimedez1 <85328736+Archimedez1@users.noreply.github.com>
2026-06-14 10:51:30 -0600

    Generating PDFs from a Spreadsheet

---
+++ Generating_a_PDF_from_Excel_Spreasheets.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from fpdf import FPDF
+
+df = pd.read_excel("food_favs_python.xlsx")
+#print(df)
+
+
+
+for index, row in df.iterrows():
+    pdf = FPDF(orientation = 'P', unit = 'pt', format = 'A4')
+    pdf.add_page()
+    
+    pdf.set_font(family='Times',style='B',size=24)
+    pdf.cell(w=0,h=50,txt=row['name'],align='C',ln=1)
+    
+    
+    for column in df.columns[1:]:
+        pdf.set_font(family='Times',style='B',size=14)
+        pdf.cell(w=100,h=25,txt=f"{column.title()}:")
+
+        pdf.set_font(family='Times',style='B',size=14)
+        pdf.cell(w=100,h=25,txt=str(row[column]),ln=1)
+        
+    
+
+    
+    
+    
+    
+    
+    pdf.output(f"{row['name']}.pdf")
+
